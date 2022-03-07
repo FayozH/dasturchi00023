e9627875d50e956aaee949be8f6b2dade139d074
FayozH <khomidov0023@gmail.com>
2022-03-07 09:24:28 -0700

    1-kunlik -30% lik uzgarish

---
+++ boot-js/boot.js
@@ -0,0 +1,10 @@
+// header navbar//       No-114(qator)
+var popoverTriggerList = [].slice.call(document.querySelectorAll('[data-bs-toggle="popover"]'))
+var popoverList = popoverTriggerList.map(function (popoverTriggerEl) {
+  return new bootstrap.Popover(popoverTriggerEl)
+})
+var popover = new bootstrap.Popover(document.querySelector('.example-popover'), {
+    container: 'body'
+  })
+  //////////////////
+
